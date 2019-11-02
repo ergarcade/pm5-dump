147e04e95f07800928c69958c0da82d0e559f872
Dean Fogarty <angrytongan@gmail.com>
2019-11-02 20:44:55 +1000

    Pull model number, machine type query (unsupported).

--- js/monitor.js
+++ js/monitor.js
@@ -17,10 +17,6 @@ const services = {
 };
 const characteristics = {
     informationService: {
-        modelNumber: {
-            id:         'ce060011-43e5-11e4-916c-0800200c9a66',
-            service:    services.information
-        },
         serialNumber: {
             id:         'ce060012-43e5-11e4-916c-0800200c9a66',
             service:    services.information
@@ -35,10 +31,6 @@ const characteristics = {
         },
         manufacturerName: {
             id:         'ce060015-43e5-11e4-916c-0800200c9a66',
-            service:    services.information
-        },
-        machineType: {
-            id:         'ce060016-43e5-11e4-916c-0800200c9a66',
             service:    services.information
         }
     },
@@ -1402,12 +1394,6 @@ class Monitor {
 
     /*
      */
-    getModelNumber() {
-        return this._getStringCharacteristicValue(characteristics.informationService.modelNumber);
-    }
-
-    /*
-     */
     getSerialNumber() {
         return this._getStringCharacteristicValue(characteristics.informationService.serialNumber);
     }
@@ -1432,26 +1418,10 @@ class Monitor {
 
     /*
      */
-    getMachineType() {
-        return this._getCharacteristic(characteristics.informationService.machineType)
-            .then(c => {
-                return c.readValue()
-            })
-            .then(v => {
-                return v.getUint8(0);
-            });
-    }
-
-    /*
-     */
     getMonitorInformation() {
         const monitorInformation = {};
 
-        return this.getModelNumber()
-            .then(modelNumber => {
-                monitorInformation.modelNumber = modelNumber;
-                return this.getSerialNumber();
-            })
+        return this.getSerialNumber()
             .then(serialNumber => {
                 monitorInformation.serialNumber = serialNumber;
                 return this.getHardwareRevision();
@@ -1466,14 +1436,11 @@ class Monitor {
             })
             .then(manufacturerName => {
                 monitorInformation.manufacturerName = manufacturerName;
-                return this.getMachineType();
-            })
-            .then(machineType => {
-                monitorInformation.machineType = machineType;
-                return Promise.resolve(monitorInformation);
+                return Promise.resolve(monitorInformation)
             })
             .catch(error => {
                 console.log(error);
+                return Promise.reject(error);
             });
     }
 };
